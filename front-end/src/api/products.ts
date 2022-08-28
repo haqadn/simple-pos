031df4eadc90d6908c904f521018f6cccd86ae9a
Adnan Haque <adnan007.id@gmail.com>
2022-08-28 16:42:33 +0600

    Show out of stock products

--- front-end/src/api/products.ts
+++ front-end/src/api/products.ts
@@ -2,6 +2,6 @@ import { API } from "./api";
 
 export default class ProductsAPI extends API {
   static async getProducts() {
-    return await this.client.get('/products?per_page=100&status=publish&stock_status=instock');
+    return await this.client.get('/products?per_page=100&status=publish');
   }
 }
