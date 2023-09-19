e7645e7ceb89ab431eb40065bc5ee54fcc1ae5a7
Adnan Haque <adnan007.id@gmail.com>
2023-09-20 00:25:46 +0600

    Change logo to Cozy Cafe

--- front-end/src/utils/config.ts
+++ front-end/src/utils/config.ts
@@ -1,3 +1,11 @@
+const simplePosSettings = window.simplePosSettings || {
+  api: {
+    base: 'https://example.com',
+    nonce: 'banana',
+  },
+  adminUrl: 'https://example.com'
+};
+
 export default {
   api: {
     base: simplePosSettings.apiBase,
