511717cfb95dee0bbcff680caf5b1cb2f9713ad4
Adnan Haque <adnan007.id@gmail.com>
2023-09-30 01:32:21 +0600

    Fix cus command

--- front-end/src/commands/add-customer-info.ts
+++ front-end/src/commands/add-customer-info.ts
@@ -7,17 +7,19 @@ export default class implements Command {
 
   parse(command: string): boolean {
     const parts = command.split(" ");
+    this.type = parts[0] as "name" | "phone" | "cus";
 
     if (["name", "phone"].includes(parts[0]) && parts.length >= 2) {
-      this.type = parts[0] as "name" | "phone" | "cus";
       this.info[this.type] = parts.slice(1).join(" ");
       return true;
     }
 
     // Shorthand command, Ex: "cus 1234567890 John Doe"
     if (parts[0] === "cus" && parts.length >= 3) {
-      this.info.phone = parts[1];
-      this.info.name = parts.slice(2).join(" ");
+      this.info = {
+        phone: parts[1],
+        name: parts.slice(2).join(" ");
+      };
       return true;
     }
     return false;
