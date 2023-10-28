9495cc393ca1ac863341c2272af62907b3e6a97c
Adnan Haque <adnan007.id@gmail.com>
2023-10-28 08:24:25 +0600

    Config update

--- front-end/forge.config.js
+++ front-end/forge.config.js
@@ -5,28 +5,28 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      platforms: ['win32'],
+      name: "@electron-forge/maker-squirrel",
+      platforms: ["win32"],
       config: {
-        name: 'CozyCafe',
+        name: "simple-pos",
       },
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
     },
     {
-      name: '@electron-forge/maker-deb',
+      name: "@electron-forge/maker-deb",
       config: {},
     },
     {
-      name: '@electron-forge/maker-rpm',
+      name: "@electron-forge/maker-rpm",
       config: {},
     },
   ],
   plugins: [
     {
-      name: '@electron-forge/plugin-auto-unpack-natives',
+      name: "@electron-forge/plugin-auto-unpack-natives",
       config: {},
     },
   ],
