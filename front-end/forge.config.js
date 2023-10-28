68153c76a9b7ed679a270b78c9db06f31f5a0f12
Adnan Haque <adnan007.id@gmail.com>
2023-10-28 08:47:04 +0600

    Build for mac as well

--- front-end/forge.config.js
+++ front-end/forge.config.js
@@ -24,6 +24,18 @@ module.exports = {
       config: {},
     },
   ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "haqadn",
+          name: "simple-pos",
+        },
+        prerelease: true,
+      },
+    },
+  ],
   plugins: [
     {
       name: "@electron-forge/plugin-auto-unpack-natives",
