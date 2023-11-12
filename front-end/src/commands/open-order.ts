6527fa99ee20b3190dac61b995b146090120f400
Adnan Haque <adnan007.id@gmail.com>
2023-11-12 11:34:10 +0600

    Update command to include disguised order id

--- front-end/src/commands/open-order.ts
+++ front-end/src/commands/open-order.ts
@@ -6,6 +6,12 @@ export default class implements Command {
 
   parse(command: string): boolean {
     const parts = command.split(" ");
+    if (parts[0] == "open" && parts[1] === "i" && parts.length === 3) {
+      const orderRef = command.split(" ")[2];
+      const orderId = parseInt(orderRef.slice(0, -2)) + parseInt(orderRef.slice(-2));
+      this.orderId = orderId.toString();
+      return true;
+    }
     if (parts[0] === "open" && parts.length === 2) {
       this.orderId = command.split(" ")[1];
       return true;
