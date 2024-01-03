fa53680dc3bebcfe8b7ed49f0ebf4564eaed9191
Adnan Haque <adnan007.id@gmail.com>
2024-01-04 00:27:56 +0600

    Remove coupon link

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -6,7 +6,6 @@
         <v-spacer class="mt-10"></v-spacer>
         <v-btn variant="flat" to="/settings" class="w-100 mb-2">Settings</v-btn>
         <v-btn variant="flat" to="/report" class="w-100 mb-2">Report</v-btn>
-        <v-btn variant="flat" to="/coupon" class="w-100 mb-2">Coupon</v-btn>
         <v-spacer class="mt-10"></v-spacer>
         <h6 v-if="recentlyClosed.length > 0">Closed:</h6>
         <v-btn
