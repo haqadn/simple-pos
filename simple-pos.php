92b8ef838e6aef4a96811c76a46dc4ae35c965ca
Adnan Haque <adnan007.id@gmail.com>
2024-01-05 17:26:06 +0600

    Add default city and postcode

--- simple-pos.php
+++ simple-pos.php
@@ -24,39 +24,56 @@ $customers->init();
 
 
 function spos_register_scripts() {
-    wp_register_style( 'simple-pos-iconfont', 'https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css', array(), '1.0.0', 'all' );
-    wp_register_style( 'simple-pos', plugins_url( 'front-end/dist/assets/style.css', __FILE__ ), array('simple-pos-iconfont'), '1.0.0', 'all' );
-    wp_register_script( 'simple-pos', plugins_url( 'front-end/dist/index.js', __FILE__ ), array(), '1.0.0', true );
-    wp_localize_script( 'simple-pos', 'simplePosSettings', array(
-        'nonce' => wp_create_nonce( 'wp_rest' ),
-        'url' => get_bloginfo( 'url' ),
-        'method' => 'nonce'
-    ) );
+	wp_register_style( 'simple-pos-iconfont', 'https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css', array(), '1.0.0', 'all' );
+	wp_register_style( 'simple-pos', plugins_url( 'front-end/dist/assets/style.css', __FILE__ ), array('simple-pos-iconfont'), '1.0.0', 'all' );
+	wp_register_script( 'simple-pos', plugins_url( 'front-end/dist/index.js', __FILE__ ), array(), '1.0.0', true );
+	wp_localize_script( 'simple-pos', 'simplePosSettings', array(
+		'nonce' => wp_create_nonce( 'wp_rest' ),
+		'url' => get_bloginfo( 'url' ),
+		'method' => 'nonce'
+	) );
 }
 add_action( 'wp_enqueue_scripts', 'spos_register_scripts' );
 
 function spos_shortcode() {
-    ob_start();
-    wp_enqueue_style( 'simple-pos-iconfont' );
-    wp_enqueue_style( 'simple-pos' );
-    wp_enqueue_script( 'simple-pos' );
-    add_filter('script_loader_tag', 'spos_add_type_attribute' , 10, 3);
-    ?>
-    <div id="pos-app"></div>
-    <?php
-    return ob_get_clean();
+	ob_start();
+	wp_enqueue_style( 'simple-pos-iconfont' );
+	wp_enqueue_style( 'simple-pos' );
+	wp_enqueue_script( 'simple-pos' );
+	add_filter('script_loader_tag', 'spos_add_type_attribute' , 10, 3);
+	?>
+	<div id="pos-app"></div>
+	<?php
+	return ob_get_clean();
 }
 add_shortcode('simple-pos', 'spos_shortcode');
 
 
 function spos_add_type_attribute($tag, $handle, $src) {
-    if ( 'simple-pos' === $handle){
-        $tag = '<script crossorigin type="module" type="text/javascript" src="' . esc_url( $src ) . '" id="simple-pos"></script>';   
-    }
-    return $tag;
+	if ( 'simple-pos' === $handle){
+		$tag = '<script crossorigin type="module" type="text/javascript" src="' . esc_url( $src ) . '" id="simple-pos"></script>';   
+	}
+	return $tag;
 }
 
 function spos_on_order_created( $order_id, $order ) {
-    $order->update_meta_data( 'wifi_password', Hotspot::generatePassword() );
+	$order->update_meta_data( 'wifi_password', Hotspot::generatePassword() );
 }
 // add_action( 'woocommerce_new_order', 'spos_on_order_created', 10, 2 );
+
+add_filter('woocommerce_checkout_get_value', function($input, $key ) {
+
+	global $current_user;
+
+	switch ($key) :
+		case 'billing_city':
+		case 'shipping_city':
+			return get_option( 'woocommerce_store_city' );
+		break;
+		case 'billing_postcode':
+		case 'shipping_postcode':
+			return get_option( 'woocommerce_store_postcode' );
+		break;
+	endswitch;
+
+}, 10, 2);
