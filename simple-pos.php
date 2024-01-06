151d8f1c9284d58b3c37ad224286cf410e843080
Adnan Haque <adnan007.id@gmail.com>
2024-01-06 19:08:42 +0600

    Version bump

--- simple-pos.php
+++ simple-pos.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Simple POS
  * Description:       A simple POS front-end for WooCommerce.
- * Version: 3.5
+ * Version: 3.6
  * Author:            Adnan Haque
  * Author URI:        https://eadnan.com
  * Domain Path:       /languages
