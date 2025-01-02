c1fb9732992532c642e094208c2d21006f5783b3
Adnan Haque <adnan007.id@gmail.com>
2025-01-02 07:46:48 -0500

    Add react query

--- next/app/providers.tsx
+++ next/app/providers.tsx
@@ -2,11 +2,16 @@
 'use client'
 
 import {NextUIProvider} from '@nextui-org/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export function Providers({children}: { children: React.ReactNode }) {
+const queryClient = new QueryClient()
+
   return (
-    <NextUIProvider>
-      {children}
-    </NextUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>
+        {children}
+      </NextUIProvider>
+    </QueryClientProvider>
   )
 }
