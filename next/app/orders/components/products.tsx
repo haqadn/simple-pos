70a8b8073b448d231611d7ddb6bae73e6a33bda5
Adnan Haque <adnan007.id@gmail.com>
2025-05-11 23:18:52 -0400

    Optimize for performance

--- next/app/orders/components/products.tsx
+++ next/app/orders/components/products.tsx
@@ -1,13 +1,22 @@
 'use client'
 import { ProductSchema } from "@/api/products";
 import { useProductsQuery } from "@/stores/products";
-import { CardBody, Divider, Table, TableBody, TableCell, TableRow, CardFooter, CardHeader, Card, TableHeader, TableColumn, Kbd } from "@heroui/react";
+import { CardBody, Divider, CardFooter, CardHeader, Card, Kbd } from "@heroui/react";
 import { useSelectedCategory } from "./selected-category";
 import { useCurrentOrderQuery, useLineItemQuery } from "@/stores/orders";
+import { useMemo } from "react";
 
 export default function Products() {
     const { data: products, isLoading } = useProductsQuery();
     const { selectedCategoryId } = useSelectedCategory();
+
+    const filteredProducts = useMemo(() => {
+        return selectedCategoryId === null 
+            ? products 
+            : products?.filter(product => 
+                product.categories?.some(category => category.id === selectedCategoryId)
+            );
+    }, [products, selectedCategoryId]);
 
     if( isLoading ) {
         return (
@@ -24,12 +33,6 @@ export default function Products() {
             </div>
         );
     }
-
-    const filteredProducts = selectedCategoryId === null 
-        ? products 
-        : products.filter(product => 
-            product.categories?.some(category => category.id === selectedCategoryId)
-        );
 
     return (
         <div className="flex flex-wrap gap-4 my-1 h-full overflow-y-auto p-5 -m-5">
@@ -71,28 +74,16 @@ const ProductCard = ({ product }: { product: ProductSchema }) => {
             </CardBody>
             <Divider />
             <CardFooter>
-                <Table 
-                    className="text-black/50" 
-                    removeWrapper 
-                    isStriped 
-                    hideHeader
-                    aria-label={`Product details for ${product.name}`}
-                >
-                    <TableHeader>
-                        <TableColumn>Details</TableColumn>
-                        <TableColumn>Value</TableColumn>
-                    </TableHeader>
-                    <TableBody>
-                        <TableRow>
-                            <TableCell>Price</TableCell>
-                            <TableCell className="text-right">{formatPrice(product.price)}</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>SKU</TableCell>
-                            <TableCell className="text-right">{product.sku ? <Kbd>{product.sku}</Kbd> : ''}</TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
+                <div className="w-full -m-4 p-4">
+                    <div className="text-black/75 text-small flex flex-row flex-start">
+                        <div className="flex-1 text-left">Price</div>
+                        <div>{formatPrice(product.price)}</div>
+                    </div>
+                    <div className="text-black/75 text-small flex flex-row py-2">
+                        <div className="flex-1 text-left">SKU</div>
+                        <div>{product.sku ? <Kbd>{product.sku}</Kbd> : ''}</div>
+                    </div>
+                </div>
             </CardFooter>
         </Card>
     );
